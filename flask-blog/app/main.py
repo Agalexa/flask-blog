c6de20fa230d1fb4f82f76ce244a92b7ba201778
Agalexa <60199755+Agalexa@users.noreply.github.com>
2020-02-12 14:15:06 -0800

    Add files via upload

--- flask-blog/app/main.py
+++ flask-blog/app/main.py
@@ -1,22 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, Response
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/home")
 def home():
     return render_template("home.html")
     
 @app.route("/about")
 def about():
-    return render_template("about.html")
-
-@app.route('/template')
-def template():
-    return render_template('template.html')    
+    return render_template("about.html") 
 
 @app.route('/random')
 def random():
     return render_template('random.html')
+
+@app.route('/<user>')
+def user(name):
+    return '<h1>Greatings, %s!</h1>' % user
+
+'''@app.route('/edit/<page>', methods = ['GET','POST'])
+def edit(page):
+    if request.method == 'GET':
+        return'''
+
+@app.route('/all')
+def all():
+    direc = os.path.join(os.path.dirname(__file__)) + '/templates/'
+    docu = os.listdir(direc)
+    this_list = []
+    for d in docu:
+        this_list.append(d.rsplit('.html',1)[0])
+    print (docu)
+    return render_template('all.html', var_pages = this_list)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
