a68ec7af51b9b5dd17f6efaf53461eb467dbe420
Agalexa <60199755+Agalexa@users.noreply.github.com>
2020-02-15 00:57:44 -0800

    Add files via upload

--- flask-blog/app/app/main.py
+++ flask-blog/app/app/main.py
@@ -1,46 +1,36 @@
 from flask import Flask, render_template, request, redirect, Response, url_for, session, flash
 import os
+from functools import wraps
 
 app = Flask(__name__)
 
 app.secret_key = 'secret key'
 
-@app.route('/')
-def blank():
-    return render_template('index.html')
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
 
-@app.route("/home")
+@app.route('/home')
 def home():
-    return render_template("home.html")
-    
-@app.route("/about")
-def about():
-    return render_template("about.html") 
+    return render_template('home.html')
 
-@app.route('/random')
-def random():
-    return render_template('random.html')
-
-@app.route('/<user>')
-def user(name):
-    return '<h1>Greatings, %s!</h1>' % user
-
-@app.route('/edit/<path>', methods = ['GET','POST'])
-def edit(path):
-    #if request.method == 'GET':
-    pathway = 'C:/Users/Drescu/Desktop/py-test/app/templates/' + path + '.html'
-    return Response(open(pathway).read(), mimetype= 'text/plain')
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')
 
 @app.route('/all')
+@login_required
 def all():
-    #direc = 'C:/Users/Drescu/Desktop/py-test/app' + '/templates/'
-    #direc = os.path.join(os.path.dirname(__file__)) + '/templates/'
-    docu = os.listdir('C:/Users/Drescu/Desktop/py-test/app/templates/') #method used to get the list of all files in directory specified
+    docu = os.listdir('C:/Users/Drescu/Desktop/app/templates/') #method used to get the list of all files in directory specified
     this_list = []
     for d in docu:
         this_list.append(d.rsplit('.',1)[0]) # uses method .rsplit to strip the .html from end of strings, appends to list for all page
-        #this_list.append(d.rsplit('.html',1)[0]) #uses .append method 
-    #print (" this is direc: " + direc)
     return render_template('all.html', var_pages = this_list) #then a for loop is used with list var_pages to iterate over each item in list and print it as hypertext link
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -56,10 +46,22 @@ def login():
     return render_template('login.html', error=error)
 
 @app.route('/logout')
+@login_required
 def logout():
     session.pop('logged_in', None) #will logout user by 'popping in true value with false'
     flash('You were logged out')
-    return redirect (url_for('all'))
+    return redirect (url_for('welcome'))
+
+@app.route('/')
+def blank():
+    return redirect (url_for('welcome'))
+
+@app.route('/edit/<path>', methods = ['GET','POST'])
+@login_required
+def edit(path):
+    #if request.method == 'GET':
+    pathway = 'C:/Users/Drescu/Desktop/app/templates/' + path + '.html'
+    return Response(open(pathway).read(), mimetype= 'text/plain')
 
 if __name__ == "__main__":
     app.run(debug=True)
